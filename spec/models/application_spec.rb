6b32ebc6c475f88e0211a29006e60127dfc91cef
shawndcarpenter <shawncarpenter.co@gmail.com>
2026-02-25 19:12:46 -0600

    Add testing for application_pet relationships and validations

---
+++ spec/models/application_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe Application, type: :model do
+  describe "relationships" do
+    it { should have_many(:application_pets) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip) }
+    it { should validate_presence_of(:address) }
+  end
+end

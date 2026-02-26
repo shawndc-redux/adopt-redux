b97c976cff7b02d2785cbcd23f5a4fc2e020b3c1
shawndcarpenter <shawncarpenter.co@gmail.com>
2026-02-25 19:14:09 -0600

    add mvc logic for displaying application show page

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,5 @@
 class Application < ApplicationRecord
-  validates :city, :state, :name, :zip, presence: true
+  validates :address, :city, :state, :name, :zip, presence: true
   enum status: {"In Progress": 0, "Pending": 1, "Accepted": 2, "Rejected": 3}
   has_many :application_pets
   has_many :pets, through: :application_pets

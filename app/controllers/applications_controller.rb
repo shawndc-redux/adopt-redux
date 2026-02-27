64d0ab51a01f66cf9556fc5ca2b965283df04ae4
shawndcarpenter <shawncarpenter.co@gmail.com>
2026-02-26 18:11:01 -0600

    add application create and new methods to controller with error handling

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,6 +3,20 @@ class ApplicationsController < ApplicationController
     @application = Application.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    application = Application.new(application_params)
+
+    if application.save
+      redirect_to "/applications/#{application.id}"
+    else
+      redirect_to "/applications/new"
+      flash[:alert] = "Error: #{error_message(application.errors)}"
+    end
+  end
+
     private
 
   def application_params

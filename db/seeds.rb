c2fb2ff45b3e33d88dee4ab064e61c8286b38017
shawndcarpenter <shawncarpenter.co@gmail.com>
2026-02-27 14:38:31 -0600

    fix factories not creating pets

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Shelter.destroy_all
+Pet.destroy_all 
+
+FactoryBot.create(:shelter)
+FactoryBot.create_list(:pet, 30)

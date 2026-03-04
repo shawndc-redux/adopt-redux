6a6213b5cae9f3210ce7b0f5862e096ff4390881
shawndcarpenter <shawncarpenter.co@gmail.com>
2026-03-04 13:26:55 -0600

    add pending_shelters code to admin shelters controller

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -1,5 +1,6 @@
 class Admin::SheltersController < ApplicationController
   def index
     @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY shelters.name desc")
+    @pending_shelters = Shelter.joins(:applications).where(applications: {status: "Pending"})
   end
 end
